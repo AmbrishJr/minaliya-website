b7387a1b25fa59ed3e2f6a1aa30137b5379284b9
Minaliya <mailme@minaliya.in>
2026-06-26 16:51:20 +0530

    Fix duplicate remotePatterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-    ],
     qualities: [50, 75, 85, 90],
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
